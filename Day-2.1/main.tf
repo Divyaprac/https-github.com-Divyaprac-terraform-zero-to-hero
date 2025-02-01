c6f544b19fe68b7df04cdf9ec593ccd909509a83
divya <Dmula4580@muleriders.saumag.edu>
2025-02-01 12:44:27 -0600

    change to value

--- Day-2.1/main.tf
+++ Day-2.1/main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./module/ec2_instance"
-  ami = "ami-04b4f1a9cf54c11d0"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0e20f6f0099481c3c"
+  ami_value = "ami-04b4f1a9cf54c11d0"
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-0e20f6f0099481c3c"
 }
+
