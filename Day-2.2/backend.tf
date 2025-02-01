46460b39fad704cfb3fe68cd7c70e20233ddf743
divya <Dmula4580@muleriders.saumag.edu>
2025-02-01 15:18:49 -0600

    uncommented

--- Day-2.2/backend.tf
+++ Day-2.2/backend.tf
@@ -1,8 +1,8 @@
-/*terraform {
+terraform {
   backend "s3" {
     bucket = "divya-s3-demo-xyz"
     key    = "divya/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform_lock"
   }
-}*/
+}
