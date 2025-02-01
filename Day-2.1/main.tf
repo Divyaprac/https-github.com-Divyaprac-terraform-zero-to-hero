56955676ae4bf08ae66119473b6735da2ff661f8
divya <Dmula4580@muleriders.saumag.edu>
2025-02-01 12:40:56 -0600

    updated

--- Day-2.1/main.tf
+++ Day-2.1/main.tf
@@ -3,9 +3,8 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
+  source = "./module/ec2_instance"
   ami = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
   subnet_id = "subnet-0e20f6f0099481c3c"
 }
-
