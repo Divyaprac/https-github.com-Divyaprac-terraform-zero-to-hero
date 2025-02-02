5b3f4b69cb2b9d750f1d17ef2b324c5373bcc0b6
divya <Dmula4580@muleriders.saumag.edu>
2025-02-02 15:45:26 -0600

    yes

--- Day-2.1/main.tf
+++ Day-2.1/main.tf
@@ -6,6 +6,6 @@ module "ec2_instance" {
   source = "./module/ec2_instance"
   ami_value = "ami-04b4f1a9cf54c11d0"
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-0e20f6f0099481c3c"
+  subnet_id_value = "subnet-0429ad5cb60c71ec6"
 }
 
