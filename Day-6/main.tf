b39012499c7e8e9f24d0e7ae206281b86da2703d
divya <Dmula4580@muleriders.saumag.edu>
2025-02-02 15:59:50 -0600

    commit

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -26,4 +26,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami = var.ami
   instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+  subnet_id = var.subnet_id
 }
