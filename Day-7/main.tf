003336d2b5590ff0c0d0a7e0cbef7b9ba53022ba
divya <Dmula4580@muleriders.saumag.edu>
2025-02-02 18:28:46 -0600

    commented

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -3,20 +3,21 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address = "54.196.138.151:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "06d49d04-f467-c50f-ebbe-40072be523bd"
+      secret_id = "2648019-2054-ac9d-acc5-be242fe303b0
+"
     }
   }
 }
 
-data "vault_kv_secret_v2" "example" {
+/*data "vault_kv_secret_v2" "example" {
   mount = "secret" // change it according to your mount
   name  = "test-secret" // change it according to your secret
 }
@@ -29,4 +30,4 @@ resource "aws_instance" "my_instance" {
     Name = "test"
     Secret = data.vault_kv_secret_v2.example.data["foo"]
   }
-}
+}*/
